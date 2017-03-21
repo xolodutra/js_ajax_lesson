78d834904b9337f9b29433721fca317cc5dc23e6
xolodutra <pavelsahnov@yandex.ru>
2017-03-21 21:48:34 +0300

    sdfsdf

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
 
     $(".load-result").bind("click", function( event ) {
         event.preventDefault();
+        $("#result").css({"top": 150});
+        $("#result").show();
         $.get("/get-result/", function (data) {
                 $("#result").html(data);
         });
@@ -13,4 +15,9 @@ $( document ).ready(function() {
         event.preventDefault();
         $("#result").animate({"top": 2000}, "slow");
     });   
+
+        $(".fade-result").bind("click", function ( event ) {
+        event.preventDefault();
+        $("#result").fadeOut("slow");
+    });
 });
