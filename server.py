9e431954193f5f101b93c117d7a69ad5a552ee5a
xolodutra <pavelsahnov@yandex.ru>
2017-03-26 19:31:34 +0300

    Пока на этом всё

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
 
 my_flask_app = Flask(__name__)
 
@@ -12,6 +12,13 @@ def index():
 def result():
     return render_template('result.html')
 
+
+@my_flask_app.route('/get-result-json/', methods=['GET'])
+def result_js():
+    data = [("Вася", "30", "$ 7830",),
+            ("Маша", "26", "$ 4830",),
+            ("Петя", "5", "$ 210",)]
+    return jsonify(sales=data)
 
 if __name__ == "__main__":
     my_flask_app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,34 @@
 $( document ).ready(function() {
 
-    $('h1').addClass('red');
+$('h1').addClass('red');
 
-    $(".load-result").bind("click", function( event ) {
-        event.preventDefault();
-        $("#result").css({"top": 150});
-        $("#result").show();
-        $.get("/get-result/", function (data) {
-                $("#result").html(data);
-        });
+$(".load-result").bind("click", function( event ) {
+    event.preventDefault();
+    $("#result").css({"top": 150});
+    $("#result").show();
+    $.getJSON("/get-result-json/", function (data) {
+        var html = '<table class="table"><tr><th>№</th>\
+                    <th>Сотрудник</th><th>Заказы</th>\
+                    <th>Деньги</th></tr>';
+        for (i=0; i< data['sales'].length; i++) {
+            html += '<tr><td>' + (i+1) + '</td><td>' 
+                + data['sales'][i][0] + '</td><td>' 
+                + data['sales'][i][1] + '</td><td>' 
+                + data['sales'][i][2] + '</td></tr>';
+        };
+        html += '</table>';
+        console.log(html);
+        $("#result").html(html);
     });
+});
 
-        $(".move-result").bind("click", function ( event ) {
-        event.preventDefault();
-        $("#result").animate({"top": 2000}, "slow");
-    });   
+    $(".move-result").bind("click", function ( event ) {
+    event.preventDefault();
+    $("#result").animate({"top": 2000}, "slow");
+});   
 
-        $(".fade-result").bind("click", function ( event ) {
-        event.preventDefault();
-        $("#result").fadeOut("slow");
-    });
+    $(".fade-result").bind("click", function ( event ) {
+    event.preventDefault();
+    $("#result").fadeOut("slow");
+});
 });
